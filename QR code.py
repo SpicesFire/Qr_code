8ee239146ddf57195db6cb0fff1a2864a7a31c74
SpicesFire <111581229+SpicesFire@users.noreply.github.com>
2023-01-10 20:26:50 +0300

    New patch
    no need to write .png in the program anymore

--- QR code.py
+++ QR code.py
@@ -3,7 +3,8 @@ This program is made by Kerem Aydın Güven
 """
 import qrcode
 import image
-link = input("Enter URL: ")
+link = input("Enter URL:")
 file_name = input("Enter file name: ")
 code=qrcode.make(link)
-code.save(file_name)
+code.save(f"{file_name}.png")
+code.show(f"{file_name}.png")
